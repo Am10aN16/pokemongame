4287ad8b534105cdaaf17f1bc15378bb461784ff
Am10aN16 <2020071013@mmmut.ac.in>
2023-06-23 02:19:32 +0530

    second

--- index.js
+++ index.js
@@ -35,12 +35,12 @@ try {
 
 
 //For production code to serve client build
-// if (process.env.NODE_ENV=== 'production') {
-//     app.use(express.static('client/build'));
-//     app.get('*',(req,res) => {
-//         res.sendFile(path.join(__dirname , 'client' , 'build' , 'index.html'))
-//     })
-// }
+if (process.env.NODE_ENV=== 'production') {
+    app.use(express.static('client/build'));
+    app.get('*',(req,res) => {
+        res.sendFile(path.join(__dirname , 'client' , 'build' , 'index.html'))
+    })
+}
 
 
 //PORT Connection
